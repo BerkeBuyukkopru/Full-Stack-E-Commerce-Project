de2b791e147817bb7d0bad1b07687b64ae7d97d6
Berke Büyükköprü <berkebuyukkopru06@gmail.com>
2025-12-12 14:00:44 +0300

    MongoDB - API

---
+++ API/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+// API/Controllers/HomeController.cs
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController] 
+public class HomeController : ControllerBase
+{
+    [HttpGet("/")] 
+    public IActionResult GetRoot()
+    {
+        return Ok("Hello ASP.NET Core (Root)"); 
+    }
+
+    [HttpGet("/api")] 
+    public IActionResult GetApiRoute()
+    {
+        return Ok("This is API Route (API)");
+    }
+}
